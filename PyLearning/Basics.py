fb159f54efb807c058fb204a4867e56e572ee188
Pingu2a <pmetheus@protonmail.com>
2025-12-08 18:14:13 +0100

    08.12.25 last

--- PyLearning/Basics.py
+++ PyLearning/Basics.py
@@ -105,18 +105,32 @@
 
 # ----------------------------------------------------------------------------------------------------------------
 
+# Training with random module
 
-# ----------------------------------------------------------------------------------------------------------------
 
 import random
 
+
+# print(random.randrange(0, 11, 2))
+# print(random.randint(0, 11))
+# print(round(random.random(), 2))
+
+list = ["Dog", "Cat", "Monkey", "Penguin", "Bear"]
+# print(random.choice(list))
+# print(random.choice(list[2]))
+# print(random.choices(list, k=2))
+
+# ----------------------------------------------------------------------------------------------------------------
+
+# import random
+
 # Training with lists (adding, removing, manipulate lists etc)
 
-colors = ["\033[1;32m", "\033[1;31m", "\033[0;35m"]
-GREEN = "\033[1;32m"
-RED = "\033[1;31m"
-END = "\033[0m"
-fruits = ["Apple","Banana","Peach","Mango","Raspberry","Strawberry"]
+# colors = ["\033[1;32m", "\033[1;31m", "\033[0;35m"]
+# GREEN = "\033[1;32m"
+# RED = "\033[1;31m"
+# END = "\033[0m"
+# fruits = ["Apple","Banana","Peach","Mango","Raspberry","Strawberry"]
 
 # print(GREEN + str(fruits) + END)
 
@@ -134,5 +148,10 @@ fruits = ["Apple","Banana","Peach","Mango","Raspberry","Strawberry"]
 # fruits.clear()
 # print(RED + str(fruits) + END)
 
-for fruit in fruits:
-    print(random.choice(colors) + fruit + END)
+# for fruit in fruits:
+#     print(random.choice(colors) + fruit + END)
+
+# if "Apple" in fruits:
+#     print(RED + "Yeah Apple is in the list ! " + END)
+
+# print(len(fruits))
