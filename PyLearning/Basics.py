8f605d521743d6990bc09479ad53ccbde03f1e31
Pingu2a <pmetheus@protonmail.com>
2025-12-08 17:06:33 +0100

    08.12.25 2nd

--- PyLearning/Basics.py
+++ PyLearning/Basics.py
@@ -15,7 +15,7 @@
 
 # ----------------------------------------------------------------------------------------------------------------
 
-# # tip calculator 
+# # tip calculator
 
 # total = float(input("What was the total bill ? $"))
 # tips = int(input("How much tip would you like to give (10, 12 or 15) ? "))
@@ -102,4 +102,37 @@
 #         print("Goodbye !")
 #         break
 
+
 # ----------------------------------------------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------------------------------------------
+
+import random
+
+# Training with lists (adding, removing, manipulate lists etc)
+
+colors = ["\033[1;32m", "\033[1;31m", "\033[0;35m"]
+GREEN = "\033[1;32m"
+RED = "\033[1;31m"
+END = "\033[0m"
+fruits = ["Apple","Banana","Peach","Mango","Raspberry","Strawberry"]
+
+# print(GREEN + str(fruits) + END)
+
+# fruits[1] = "Orange" --> Change value index 1
+
+# Add in a list
+# fruits.append("Carot")
+# fruits.extend(["peeeach", "pineeeeapple"])
+# fruits[1:1] = ["Tomato", "Laituce", "Chiken"]
+
+# Delete in a list
+# fruits.remove("Raspberry")
+# fruits.pop(1)
+# fruits.pop()
+# fruits.clear()
+# print(RED + str(fruits) + END)
+
+for fruit in fruits:
+    print(random.choice(colors) + fruit + END)
